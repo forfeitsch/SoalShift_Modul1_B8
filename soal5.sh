a4960cc3b4b137ffd53ab8ef61df400e7a248883
Kevash <36850442+forfeitsch@users.noreply.github.com>
2019-02-22 15:30:46 +0700

    Update soal5.sh

--- soal5.sh
+++ soal5.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 
-awk '{if ($0 ~ /cron/ && $0 !~ /sudo/ && NF < 13) print $0 }' /var/log/syslog >> ~/soal5.log 2>&1
+awk '{if ($0 ~ /cron/ && $0 !~ /sudo/ && NF < 13) print $0 }' /var/log/syslog >> ~/modul1/soal5.log 2>&1
