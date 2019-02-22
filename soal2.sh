5ab4d05fea72fbf9112a105b3804fa24e560f7d0
elknhns <33386421+elknhns@users.noreply.github.com>
2019-02-22 17:19:00 +0700

    Update soal2.sh

--- soal2.sh
+++ soal2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "2.a"
-awk -F, '{if($7=="2012") a[$1]+=$10;} END{for(i in a) print a[i]","i}' WA_Sales_Products_2012-14.csv | sort -n -r | head -n 1 | awk -F, '{print $2}'
+awk -F "," '{if ($7 == 2012) i[$1]+=$10} END {for (x in i) if(max<i[x]) {max=i[x]; country=x} print max, country}' /home/forfeitsch/sisop2019/prak_mod1/WA_Sales_Products_2012-14.csv
 echo ""
 echo "2.b"
-awk -F, '{if($1=="United States" && $7=="2012") a[$4]+=$10;} END{for(i in a) print a[i]","i}' WA_Sales_Products_2012-14.csv | sort -n -r | head -n 3 | awk -F, '{print $2}'
+awk -F "," '{if ($1 == "United States" && $7 == 2012) j[$4]+=$10} END {for (x in j) {print j[x], x}}' /home/forfeitsch/sisop2019/prak_mod1/WA_Sales_Products_2012-14.csv | sort -nr | head -n 3 | awk '{print $2" "$3}'
 echo ""
 echo "2.c"
-awk -F, '{if($1=="United States" && $7=="2012" && ($4=="Personal Accessories" || $4=="Camping Equipment" || $4=="Outdoor Protection")) a[$6]+=$10;} END{for(i in a) print a[i]","i}' WA_Sales_Products_2012-14.csv | sort -n -r | head -n 3 | awk -F, '{print $2}'
+awk -F "," '{if ($1 == "United States" && $7 == 2012 && ($4 == "Personal Accessories" || $4 == "Camping Equipment" || $4 == "Outdoor Protection")) k[$6]+=$10} END {for (x in k) {print k[x], x}}' /home/forfeitsch/sisop2019/prak_mod1/WA_Sales_Products_2012-14.csv | sort -nr | head -n 3 | awk '{print $2" "$3}'
 
